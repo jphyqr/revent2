d14e80178b9b1e4b5f188c6a12f1c9a1e13b1713
John Hashem <jphyqr@gmail.com>
2018-10-23 09:45:41 -0600

    end of section 12

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -3,14 +3,16 @@ import { reducer as FormReducer } from 'redux-form';
 import testReducer from '../../features/testarea/testReducer';
 import eventReducer from '../../features/event/eventReducer';
 import modalsReducer from '../../features/modals/modalReducer';
-import authReducer from '../../features/auth/authReducer';
+import authReducer from '../../features/auth/authReducer';  
+import asyncReducer from '../../features/async/asyncReducer'
 
 const rootReducer = combineReducers({
   test: testReducer,
   events: eventReducer,
   form: FormReducer,
   modals: modalsReducer,
-  auth: authReducer
+  auth: authReducer,
+  async: asyncReducer
 })
 
 export default rootReducer
---
+++ src/features/async/asyncReducer.jsx
@@ -0,0 +1,30 @@
+
+import {ASYNC_ACTION_START,ASYNC_ACTION_FINISH, ASYNC_ACTION_ERROR} from './asyncConstants'
+import {createReducer} from '../../app/common/util/reducerUtil'
+import { asyncActionFinish } from './asyncActions';
+
+
+const initialState = {
+    loading: false
+}
+
+
+export const asyncActionStarted = (state,payload) =>{
+     return {...state, loading :true}
+}
+
+
+export const asyncActionFinished = (state, payload) => {
+    return {...state, loading:false}
+}
+
+export const asyncActionError = (state, payload) => {
+    return {...state, loading:false}
+}
+
+
+export default createReducer(initialState, {
+    [ASYNC_ACTION_START] : asyncActionStarted,
+    [ASYNC_ACTION_ERROR] : asyncActionError,
+    [ASYNC_ACTION_FINISH] : asyncActionFinish
+})
